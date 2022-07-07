29b0ba73b724f6b9f8b60ccb5d8aa8e3bdc030b4
jrj-sys <jamesjamel99@gmail.com>
2022-07-06 19:10:21 -0500

    translate as utils, will move

---
+++ utils/translator.js
@@ -0,0 +1,31 @@
+// require Google Cloud Translation API 
+const { Translate } = require('@google-cloud/translate').v2;
+// dotenv for env variables
+require('dotenv').config();
+
+// parse env CREDENTIALS for authorization on Translate instantiation 
+const CREDENTIALS = JSON.parse(process.env.CREDENTIALS);
+
+// instantiate a new Translate object
+const translate = new Translate({
+  credentials: CREDENTIALS,
+  projectId: CREDENTIALS.projectId});
+
+
+async function translate() {
+  // text will be equal to user input
+  const text = 'redação';
+
+  //  target will be equal to receiving user's selected preferred language
+  const target = 'en'
+
+  // async translation 
+  const [translation] = await translate.translate(text, target);
+  console.log(`Text: ${text}`);
+  console.log(`Translation: ${translation}`);
+}
+
+// call the translator on load OR export
+// translator();
+
+module.exports = translate;
