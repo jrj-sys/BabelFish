3f1b39e96b5228ccf45605cf73124f9880437900
Steffen568 <sgonzalez568@gmail.com>
2022-07-07 22:08:33 -0500

    added image file upload and react to profile. NOT TESTED

--- client/src/components/Profile/index.js
+++ client/src/components/Profile/index.js
@@ -1,6 +1,22 @@
 import React from 'react';
 
 function ProfilePage() {
+    const uploadedImage = React.useRef(null);
+    const imageUploader = React.useRef(null);
+  
+    const handleImageUpload = e => {
+      const [file] = e.target.files;
+      if (file) {
+        const reader = new FileReader();
+        const { current } = uploadedImage;
+        current.file = file;
+        reader.onload = e => {
+          current.src = e.target.result;
+        };
+        reader.readAsDataURL(file);
+      }
+    };
+
     return(
         <section class="vh-100" style="background-color: rgb(231, 131, 37);">
             <div class="container py-5 h-100">
@@ -9,8 +25,32 @@ function ProfilePage() {
                         <div class="card" style="border-radius: 15px;">
                             <div class="card-body text-center">
                                 <div class="mt-3 mb-4">
-                                    <img src="https://mdbcdn.b-cdn.net/img/Photos/new-templates/bootstrap-chat/ava2-bg.webp"
-                                        class="rounded-circle img-fluid" style="width: 100px;" />
+                                    <input 
+                                        type='file' 
+                                        accept='image/*' 
+                                        onChange={handleImageUpload}
+                                        ref={imageUploader}
+                                        multiple='false' 
+                                        style={{
+                                            display: 'none'
+                                        }}/>
+                                    <div
+                                        style={{
+                                        height: "60px",
+                                        width: "60px",
+                                        border: "1px dashed black"
+                                        }}
+                                        onClick={() => imageUploader.current.click()}
+                                    >
+                                        <img
+                                        ref={uploadedImage}
+                                        style={{
+                                            width: "100%",
+                                            height: "100%",
+                                            position: "absolute"
+                                        }}
+                                        />
+                                    </div>
                                 </div>
                                 <h4 class="mb-2">Your name here</h4>
                                 <p class="text-muted mb-4">@Programmer <span class="mx-2">|</span> <a
