245ba9c15600d522dbf23679e1f78c69a6d93233
PaulS <psoliz333@gmail.com>
2022-07-08 22:48:15 -0500

    models pulled from jamel

--- client/src/components/Homepage/index.js
+++ client/src/components/Homepage/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import './style.css'
 import { Card, Avatar, CardActionArea } from '@mui/material'
+import { Link } from 'react-router-dom'
 
 import avatarImgOne from "../../assets/images/imgExamples/WIN_20220630_14_33_40_Pro[234].jpg"
 import avatarImgTwo from "../../assets/images/imgExamples/ed-panopio-img-2036.jpg"
@@ -10,33 +11,28 @@ function Homepage() {
         <div>
             <h2>conversation</h2>
 
-            <div className='conversation'>
-                <Card className='conversationCard'>
-                    <CardActionArea>
-                        <div className='convo-title'>
-                            <Avatar alt="" src={avatarImgOne} className="convo-avatar"></Avatar>
-                            <p className='convo-name'>Paul</p>
-                            <p className='convo-read'>4:20pm</p>
-                        </div>
-                        <p className='convo-message'>Message!</p>
-                    </CardActionArea>
-                </Card>
-            </div>
+            <Card className='convo-card' style={{ backgroundColor: "lightgrey" }}>
+                <CardActionArea>
+                    <div className='convo-title'>
+                        <Avatar alt="" src={avatarImgOne} className="convo-avatar"></Avatar>
+                        <p className='convo-name'>Paul</p>
+                        <p className='convo-read'>4:20pm</p>
+                    </div>
+                    <p className='convo-message'>Supp!</p>
+                </CardActionArea>
+            </Card>
 
 
-
-            <div className='conversation'>
-                <Card className='conversationCard'>
-                    <CardActionArea>
-                        <div className='convo-title'>
-                            <Avatar alt="" src={avatarImgTwo} className="convo-avatar"></Avatar>
-                            <p className='convo-name'>Paul</p>
-                            <p className='convo-read'>4:20pm</p>
-                        </div>
-                        <p className='convo-message'>Message!</p>
-                    </CardActionArea>
-                </Card>
-            </div>
+            <Card className='convo-card' style={{ backgroundColor: "lightgrey" }}>
+                <CardActionArea>
+                    <div className='convo-title'>
+                        <Avatar alt="" src={avatarImgTwo} className="convo-avatar"></Avatar>
+                        <p className='convo-name'>Sussy</p>
+                        <p className='convo-read'>1:11pm</p>
+                    </div>
+                    <p className='convo-message'>Message! 👻</p>
+                </CardActionArea>
+            </Card>
         </div >
     )
 }
