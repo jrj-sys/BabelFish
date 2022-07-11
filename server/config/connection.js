746bec48baddc763c3d1276c9f589951c07e6f46
Xan <tsadiktalmudim@gmail.com>
2022-07-10 21:58:38 -0500

    changed server process.env

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,7 +1,10 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
-mongoose.connect(process.env.MONGODB_URL || 'mongodb://localhost:27017/BabelFish', {
-  useNewUrlParser: true,
-});
+mongoose.connect(
+  process.env.MONGODB_URL || "mongodb://127.0.0.1:27017/BabelFish",
+  {
+    useNewUrlParser: true,
+  }
+);
 
 module.exports = mongoose.connection;
