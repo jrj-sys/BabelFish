79bc05b35b54392c99d6ae63e0d5052705b88c1e
cameronstroup <90347622+cameronstroup@users.noreply.github.com>
2022-07-11 20:52:35 -0500

    fixed css

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,6 @@
 import React from "react";
 import Homepage from "./components/Homepage";
-import logo from "./logo.svg";
+
 import "./App.css";
 import LoginPage from "./components/login";
 
@@ -8,10 +8,10 @@ function App() {
   return (
     <div>
       <Homepage></Homepage>
-    <div className="App">
-      {" "}
-      <LoginPage />{" "}
-    </div>
+      <div className="App">
+        {" "}
+        <LoginPage />{" "}
+      </div>
     </div>
   );
 }
