9915f7a5f62d2d01179bb870485b130078be7315
Steffen568 <sgonzalez568@gmail.com>
2022-07-11 21:19:59 -0500

    npm/matUi error. profile page not working

--- client/src/components/Profile/index.js
+++ client/src/components/Profile/index.js
@@ -1,8 +1,28 @@
 import React from 'react';
+// import ReactDOM from 'react-dom/client';
+import { styled } from '@mui/material/styles';
+import {Card, Typography } from '@mui/material';
+import CardMedia from '@mui/material/CardMedia';
+import CardContent from '@mui/material/CardContent';
+// import Typography from '@mui/material/Typography';
+
+
+
+// const useStyles = makeStyles({
+//     root: {
+//       maxWidth: 345
+//     },
+//     media: {
+//       height: "100px"
+//     }
+//   });
+
 
 function ProfilePage() {
     const uploadedImage = React.useRef(null);
     const imageUploader = React.useRef(null);
+
+    // const classes = useStyles()
   
     const handleImageUpload = e => {
       const [file] = e.target.files;
@@ -18,51 +38,34 @@ function ProfilePage() {
     };
 
     return(
-        <section class="vh-100" style="background-color: rgb(231, 131, 37);">
-            <div class="container py-5 h-100">
-                <div class="row d-flex justify-content-center align-items-center h-100">
-                    <div class="col-md-12 col-xl-4">
-                        <div class="card" style="border-radius: 15px;">
-                            <div class="card-body text-center">
-                                <div class="mt-3 mb-4">
-                                    <input 
-                                        type='file' 
-                                        accept='image/*' 
-                                        onChange={handleImageUpload}
-                                        ref={imageUploader}
-                                        multiple='false' 
-                                        style={{
-                                            display: 'none'
-                                        }}/>
-                                    <div
-                                        style={{
-                                        height: "60px",
-                                        width: "60px",
-                                        border: "1px dashed black"
-                                        }}
-                                        onClick={() => imageUploader.current.click()}
-                                    >
-                                        <img
-                                        ref={uploadedImage}
-                                        style={{
-                                            width: "100%",
-                                            height: "100%",
-                                            position: "absolute"
-                                        }}
-                                        />
-                                    </div>
-                                </div>
-                                <h4 class="mb-2">Your name here</h4>
-                                <p class="text-muted mb-4">@Programmer <span class="mx-2">|</span> <a
-                                    href="#!">test@gmail.com</a></p>
-                                <button type="button" class="btn btn-primary btn-rounded btn-lg">
-                                    Message now
-                                </button>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            </div>
-    </section>
+        <Card className={classes.root}>
+            <input 
+                type='file' 
+                accept='image/*' 
+                onChange={handleImageUpload}
+                ref={imageUploader}
+                multiple='false' 
+                style={{
+                    display: 'none'
+                }}/>
+                onClick={() => imageUploader.current.click()}
+            <CardMedia
+                className={classes.media}
+                image={uploadedImage}
+                title="Profile image for user"
+            />
+        <CardContent>
+            <Typography gutterBottom variant="h5" component="h2">
+                CardMedia Example
+            </Typography>
+            <Typography variant="body2" color="textSecondary" component="p">
+                The CardMedia component sets a background image to cover available
+                space.
+            </Typography>
+        </CardContent>
+        </Card>
     )
 }
+
+
+export default ProfilePage;
