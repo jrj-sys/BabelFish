b5fcbf52d2718817c7dad40d3c4e0d7df59a269d
Xan <tsadiktalmudim@gmail.com>
2022-07-11 21:16:43 -0500

    added some routes to app

--- client/src/App.js
+++ client/src/App.js
@@ -1,16 +1,28 @@
 import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Homepage from "./components/Homepage";
 import "./App.css";
 import LoginPage from "./components/login";
+import { Drawer } from "@mui/material";
 
 function App() {
   return (
     <div>
-      <Homepage></Homepage>
-      <div className="App">
-        {" "}
-        <LoginPage />{" "}
-      </div>
+      <Drawer />
+      <Router>
+        <Routes>
+          <Route path="/" element={<Homepage />} />
+          <Route path="/login" element={<LoginPage />} />
+          <Route path="/chat" element={<Signup />} />
+          <Route path="/profile" element={<Profile />} />
+          <Route path="/thought/:id" element={<SingleThought />} />
+          <Route path="*" element={<NoMatch />} />
+        </Routes>
+        <div className="App">
+          {" "}
+          <LoginPage />{" "}
+        </div>
+      </Router>
     </div>
   );
 }
