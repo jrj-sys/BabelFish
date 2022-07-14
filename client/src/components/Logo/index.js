b3126f39bb6e7d3abd9b38db88d2725a820264e2
Xan <tsadiktalmudim@gmail.com>
2022-07-13 20:54:39 -0500

    added Logo component

---
+++ client/src/components/Logo/index.js
@@ -0,0 +1,12 @@
+import React from "react";
+import babelfishnobg from "../../assets/images/babelfishnobg";
+
+function Logo() {
+  return (
+    <div>
+      <img src={babelfishnobg} alt="The babelfish logo!" />
+    </div>
+  );
+}
+
+export default Logo;
