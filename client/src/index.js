b1e8b82737b7f6656cbe14a1d03defbb56ac5a63
jrj-sys <jamesjamel99@gmail.com>
2022-07-17 21:04:22 -0500

    heroku push

--- client/src/index.js
+++ client/src/index.js
@@ -1,9 +1,10 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
+import React from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import App from "./App";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const rootElement = document.getElementById("root");
+const root = createRoot(rootElement);
 root.render(
   <React.StrictMode>
     <App />
