4bf4ef868249e2bf107e71915b82a46860eda3a6
jrj-sys <jamesjamel99@gmail.com>
2022-07-17 20:28:37 -0500

    push for heroku

--- server/utils/translator.js
+++ server/utils/translator.js
@@ -4,17 +4,17 @@ const { Translate } = require('@google-cloud/translate').v2;
 require('dotenv').config();
 
 // parse env CREDENTIALS for authorization on Translate instantiation 
-// const CREDENTIALS = JSON.parse(process.env.CREDENTIALS);
+const CREDENTIALS = JSON.parse(process.env.CREDENTIALS);
 
 // instantiate a new Translate object
-// const translate = new Translate({
-//   credentials: CREDENTIALS,
-//   projectId: CREDENTIALS.projectId});
+const translate = new Translate({
+  credentials: CREDENTIALS,
+  projectId: CREDENTIALS.projectId});
 
 
 async function translator(text, target) {
   // async translation ........ text = user message, target = user preferredLang
-  // const translation = await translate.translate(text, target);
+  const translation = await translate.translate(text, target);
   // take the 0th index because the translate API returns an object and React doesn't like that
   console.log(`Text: ${text}`);
   console.log(`Translation: ${translation[0]}`);
